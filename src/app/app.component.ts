786886ca5d1f2ae00f01d7fbcf9155e822ced16b
bhaskar <jhabhaskar000@gmail.com>
2019-01-17 20:45:41 +0530

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ 
+	click(){
+		alert("clicked");
+	}
+
+ title = "i am component from ts"; 
 }
+ 
